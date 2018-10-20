5037a9176999516720e388f05e45c27429446bb0
Doua Halwa <duaah1980@gmail.com>
2018-10-20 16:23:08 +0200

    coditional rendering

--- guardian-app/src/pages/Login.js
+++ guardian-app/src/pages/Login.js
@@ -2,73 +2,89 @@ import React, { Component } from "react";
 import Sites from "./Sites.js";
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users:[],
-      username: '',
-      usernameValid: false,
-      password: '',
-      passwordValid: false,
-      submitDisabled: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            username: '',
+            usernameValid: false,
+            password: '',
+            passwordValid: false,
+            submitDisabled: true,
+            hideSites: true
+
+        };
     };
-  };
-  handleChangeUsername = (e) => {
-    let usernameValid = e.target.value.length > 6 ? true : false;
-    let submitValid = this.state.passwordValid && usernameValid;
-    this.setState({
-      username: e.target.value,
-      usernameValid: usernameValid,
-      submitDisabled: !submitValid
-    })
-  };
-  handleChangePassword = (e) => {
-    let passwordValid = e.target.value.length > 6 ? true : false;
-    let submitValid = this.state.usernameValid && passwordValid;
-    this.setState({
-      password: e.target.value,
-      passwordValid: passwordValid,
-      submitDisabled: !submitValid
-    })
-  };
- 
- handleSubmit = (e) => {
-    e.preventDefault();
-       fetch('/users')
-        .then(res => res.json())
-        .then(users => this.setState({ users }))
-        .catch(error => console.error('Error:', error));
- }
+    handleChangeUsername = (e) => {
+        let usernameValid = e.target.value.length > 6 ? true : false;
+        let submitValid = this.state.passwordValid && usernameValid;
+        this.setState({
+            username: e.target.value,
+            usernameValid: usernameValid,
+            submitDisabled: !submitValid
+        })
+    };
+    handleChangePassword = (e) => {
+        let passwordValid = e.target.value.length > 6 ? true : false;
+        let submitValid = this.state.usernameValid && passwordValid;
+        this.setState({
+            password: e.target.value,
+            passwordValid: passwordValid,
+            submitDisabled: !submitValid
+        })
+    };
 
-  render() {
-    const { users } = this.state;
+    handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }))
+            .catch(error => console.error('Error:', error));
+    }
+    handleClick = (e) => {
+        let newHideSites = !this.state.hideSites;
+        this.setState({
+            hideSites: newHideSites
+        })
+    }
 
-    return (
-      <div >
-        <div className="login-page">
-          <div className="login-form">
-            <form onSubmit={this.handleSubmit}>
+    render() {
+        const { users } = this.state;
+        if (this.state.hideSites) {
+            return (
+                <div >
+                    <div className="login-page">
+                        <div className="login-form">
+                            <form onSubmit={this.handleSubmit}>
 
-              <div >
-                <label>username</label><br/>
-                <input type="text" value={this.state.email} onChange={this.handleChangeUsername} />
-              </div>
+                                <div >
+                                    <label>username</label><br />
+                                    <input type="text" value={this.state.email} onChange={this.handleChangeUsername} />
+                                </div>
 
-              <div>
-               <label>password</label><br/>
-                <input type="text" value={this.state.password} onChange={this.handleChangePassword} />
-              </div>
+                                <div>
+                                    <label>password</label><br />
+                                    <input type="text" value={this.state.password} onChange={this.handleChangePassword} />
+                                </div>
 
-              <div className="login-form-submit">
-                <input type="submit" value="Login"  disabled={this.state.submitDisabled}/>
-              </div>
-              <Sites />
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                                <div className="login-form-submit">
+                                    <input type="submit" value="Login" disabled={this.state.submitDisabled} onClick={this.handleClick} />
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <Sites />
+                </div>
+            )
+        }
+
+    }
 }
 
 export default Login;
