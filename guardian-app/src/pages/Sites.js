4c38f157b3ba1bacb49493347753a9d6c313456a
Doua Halwa <duaah1980@gmail.com>
2018-10-20 15:04:08 +0200

    add sites page

---
+++ guardian-app/src/pages/Sites.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Sites extends Component {
+  // Initialize the state
+  constructor(props) {
+    super(props);
+    this.state = {
+      sites: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const getData = await fetch('/sites')
+        .then(res => res.json())
+        .then(sites => this.setState({ sites }))
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  render() {
+    const { sites } = this.state;
+
+    return (
+      <div className="App">
+        <h1>List of sites</h1>
+        <ul>
+          {sites.map((site) =>
+            <li >{site.title}</li>
+          )}
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default Sites;
