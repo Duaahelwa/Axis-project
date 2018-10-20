835f43a9586ea5100623e0adfbb6697fb40279e2
Doua Halwa <duaah1980@gmail.com>
2018-10-20 09:57:59 +0200

    create project structure

---
+++ guardian-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+
+let destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <p> Sites </p>
+        <App />
+    </div>, destination
+);
