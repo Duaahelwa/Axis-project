bd96a743ee84b392871fcefaf83486d32a7b9379
Doua Halwa <duaah1980@gmail.com>
2018-10-21 12:07:28 +0200

    add password authantification

--- guardian-app/src/components/LoginAuthentication.js
+++ guardian-app/src/components/LoginAuthentication.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
 import GetAllSites from "./GetAllSites";
+import Login from "../pages/Login";
 
 
 class LoginAuthentication extends Component {
 
 
     render() {
-        const users  = this.props.users;
-        const username  = this.props.username;
+
+        const users = this.props.users;
+        const username = this.props.username;
         const password = this.props.password;
-        const authenticatedUser = users.find((user)=> user.username === username);
-        if((authenticatedUser.username === username) && (authenticatedUser.password === password) ){
+
+        const logInUser = users.find((user) => (user.username === username) && (user.password===password))
+         
+
+        if (logInUser == null) {
+            alert("user is not registered");
             return (
-                <div >
-                    <GetAllSites username={this.props.username} />
-                </div>
+                <Login users={this.props.users}/>
             );
-
-        }else{
-            alert("user or password are not correct")
+        } else  {
+            return (
+                <GetAllSites username={this.props.username} />
+            );
+            
+            
         }
-       
-
-
     }
 }
 
