512b79d66591dee7e7b9942620f1e63b8a3d4cdd
Doua Halwa <duaah1980@gmail.com>
2018-10-22 01:14:41 +0200

    refactor

---
+++ guardian-app/src/services/DeviceServices/Devices.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import DevicesPage from "../../pages/DevicesPage";
+
+
+class Devices extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            devices: [],
+        };
+    };
+ 
+    async componentDidMount() {
+        try {
+            await fetch('/devices')
+                .then(res => res.json())
+                .then(devices => this.setState({ devices }))
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    render() {
+        const sites = this.props.sites;
+        const siteId = this.props.siteId;
+       
+            return (
+                <div >
+                   <DevicesPage sites={this.props.sites} devices={this.state.devices} siteId={this.props.siteId} />
+                </div>
+            );
+        
+
+    }
+}
+
+export default Devices;
